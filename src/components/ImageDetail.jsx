1bf48e27aaa0e56c0b431d5aaee33cc5326b084a
Gabriel Tomsic <gabriel@macbook.hsd1.ut.comcast.net>
2022-12-10 10:56:47 -0700

    Adding image component in react native

--- src/components/ImageDetail.jsx
+++ src/components/ImageDetail.jsx
@@ -1,14 +1,28 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 import React from "react";
 
-const ImageDetail = () => {
+const ImageDetail = ({ title, imageSource, score }) => {
   return (
-    <View>
-      <Text>ImageDetail</Text>
+    <View style={styles.container}>
+      <Image source={imageSource} />
+      <View style={styles.textView}>
+        <Text>{title}</Text>
+        <Text>Score of: {score}</Text>
+      </View>
     </View>
   );
 };
 
 export default ImageDetail;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    marginVertical: 3,
+  },
+  textView: {
+    marginLeft: 10,
+  },
+});
--- src/screens/ImageScreen.jsx
+++ src/screens/ImageScreen.jsx
@@ -5,9 +5,21 @@ import ImageDetail from "../components/ImageDetail";
 const ImageScreen = () => {
   return (
     <View>
-      <ImageDetail />
-      <ImageDetail />
-      <ImageDetail />
+      <ImageDetail
+        title="Forest"
+        imageSource={require("../../assets/images/forest.jpg")}
+        score={9}
+      />
+      <ImageDetail
+        title="Beach"
+        imageSource={require("../../assets/images/beach.jpg")}
+        score={7}
+      />
+      <ImageDetail
+        title="Moutain"
+        imageSource={require("../../assets/images/mountain.jpg")}
+        score={10}
+      />
     </View>
   );
 };
